abd3d109bcd2929d756aef4da8711222db13e730
Thomas Schaber <tommy.schaber@gmail.com>
2021-04-26 18:31:11 -0400

    added main functionality

--- managePass.js
+++ managePass.js
@@ -1,8 +1,25 @@
-var user = document.getElementsByName("user");
-var pass = document.getElementsByName("pass");
-document.getElementsByName("test").innerHTML = "test"
+var form1 = document.userAndPass;
+var service = form1.service;
+var user = form1.user;
+var pass = form1.pass;
+var store = [];
+var listTag = document.getElementById("list");
 
-function printUser(){
-    document.getElementsByName("test").innerHTML = "test"
-    document.write(user + " " +pass);
+
+function printList(){
+   var passStr = ""
+    for(var x = 0; x < store.length; x++){
+        passStr += store[x];
+        passStr += "<br><br>"
+   }
+   listTag.innerHTML = passStr 
 }
+
+function addToList(){
+    store.push("<Strong>Login for: " + pass.value + "</Strong><br>Username: "+ user.value + "<br>Password: "  + pass.value);
+    service.value = "";
+    user.value = "";
+    pass.value = "";
+    printList();
+}
+
